c48db8f67e5da11d85ce5b58fb4e4a0db7c8bd3e
Michael Koval <mkoval@cs.cmu.edu>
2015-08-28 15:46:07 -0400

    Also install git.

--- setup-vim.sh
+++ setup-vim.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 set -o
 
+sudo apt-get install build-essential cmake git
+sudo apt-get install python-dev
+
 cd "${HOME}"
 mkdir -p .vim/bundle
 git clone https://github.com/VundleVim/Vundle.vim.git .vim/bundle/Vundle.vim
 vim -c PluginInstall
-
-sudo apt-get install build-essential cmake
-sudo apt-get install python-dev
 
 cd .vim/bundle/YouCompleteMe
 ./install.py --clang-completer
